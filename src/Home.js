cb286426867eb3e07f1cdb18290c1da3e16ef55a
Austin Liu <austin.liu05@yahoo.com>
2024-01-21 18:55:37 -0500

    connected with flask

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,37 @@
 import './Home.css'
+import { useState } from 'react'
 
 const Home = () => {
+    const [data, setData] = useState([{}])
+    const sendData = (data) => {
+        fetch("http://127.0.0.1:5000/test", {
+            method: "POST",
+            cache: "no-store",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+            .then(
+                res => res.json())
+            .then(responseData => {
+                console.log("Server Response:", responseData);
+                responseData = JSON.parse(responseData)
+            })
+            .catch(error => {
+                console.error("Error sending data:", error);
+            });
+    };
+    const handleButtonClick = () => {
+        let data = {"test": "hi"}
+        sendData(data)
+    };
     return (
         <div>
             <h1 className='title'>AutoDCF</h1>
 
             <div className='userInput'>
-                <h2>Enter ticker symbol:</h2>
-                <h2>Enter start fiscal year:</h2>
-                <h2>Enter end fiscal year:</h2>
+                <button onClick={handleButtonClick}></button>
             </div>
         </div>
 
